8503ed0071c55cf5f47d37095608d9eb5775153b
Bill Rhoads <Bill_Rhoads@outlook.com>
2021-11-11 21:41:23 -0800

    commit to github

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Bill Rhoads" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
@@ -20,4 +20,64 @@ document.getElementById('add-gb').addEventListener('click', function() {
     // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// TODO: Hook up event listeners for the rest of the buttons//Gingerbread 
+
+let gingerMinus = document.querySelector("#minus-gb")
+
+let gingerBreadQty = 0
+gingerMinus.addEventListener("click", function(){
+    gingerBreadQty = gingerBreadQty - 1
+    document.querySelector("#qty-gb").textContent = gingerBreadQty
+
+
+})
+let gingerAdd = document.querySelector("#add-gb")
+
+
+gingerAdd.addEventListener("click", function(){
+    gingerBreadQty = gingerBreadQty + 1
+    document.querySelector("#qty-gb").textContent = gingerBreadQty
+
+})
+
+//Chocolate Chip
+let chocolateChipMinus = document.querySelector("#minus-cc")
+
+let chocolateChipQty = 0
+chocolateChipMinus.addEventListener("click", function(){
+    chocolateChipQty = chocolateChipQty - 1
+    document.querySelector("#qty-cc").textContent = chocolateChipQty
+
+
+})
+let chocolateChipAdd = document.querySelector("#add-cc")
+
+
+chocolateChipAdd.addEventListener("click", function(){
+    chocolateChipQty = chocolateChipQty + 1
+    document.querySelector("#qty-cc").textContent = chocolateChipQty
+
+})
+
+//Sugar Sprinkle
+let sugarMinus = document.querySelector("#minus-sugar")
+
+let sugarQty = 0
+sugarMinus.addEventListener("click", function(){
+    sugarQty = sugarQty - 1
+    document.querySelector("#qty-sugar").textContent = sugarQty
+
+
+})
+let sugarAdd = document.querySelector("#add-sugar")
+
+
+sugarAdd.addEventListener("click", function(){
+    sugarQty = sugarQty + 1
+    document.querySelector("#qty-sugar").textContent = sugarQty
+
+
+})
+
+let total = document.querySelector("qty-total")
+let totalQty = "qty-gb" + "qty-cc" + "qty-sugar"
